16c33351ce7333844094d47bbb89b11c9306fd8d
Eliza <eanis@uwo.ca>
2025-05-26 23:25:35 -0400

    Add Two Sum (Blind 75 - Java)

---
+++ twoSum.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        for (int i=0; i<=nums; i++){
+            if (num[i] + num[i+1]) == target){
+                return num[i];
+            else{
+                return 0;
+            }
+            }
+
+        }
+    }
+}
